96d7d089447f4fe6eb783589a0e5547ebc2a09ba
arwildo <arwildo@gmail.com>
2019-11-16 13:14:09 +0800

    initial

---
+++ character_counter.py
@@ -0,0 +1,21 @@
+#!/bin/python3
+
+def charCounter(inputStr):
+    inputStr = inputStr.lower()
+    dict = {}
+
+    for char in inputStr:
+        keys = dict.keys()
+        if char in keys:
+            dict[char] += 1
+        else:
+            dict[char] = 1
+    return dict
+
+
+userStr = str("Test")
+print(charCounter(userStr))
+
+
+# Output: {'t': 2, 'e': 1, 's': 1}
+
